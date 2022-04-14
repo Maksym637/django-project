5c2d129d508f363b2b9ba5780e01408216d59336
Maksik <maksym.oliinyk.knm.2020@lpnu.ua>
2022-04-14 18:19:04 +0300

    commit views

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -80,7 +80,7 @@ def home(request):
 
     topics = Topic.objects.all()
     room_count = rooms.count()
-    room_messages = Message.objects.all()
+    room_messages = Message.objects.filter(Q(room__topic__name__icontains=q))
 
     context = {'rooms': rooms, 'topics': topics, 
                'room_count': room_count, 'room_messages': room_messages}
@@ -89,7 +89,7 @@ def home(request):
 
 def room(request, pk):
     room = Room.objects.get(id=pk)
-    room_messages = room.message_set.all().order_by('-created')
+    room_messages = room.message_set.all()
     participants = room.participants.all()
 
     if request.method == 'POST':
@@ -106,6 +106,17 @@ def room(request, pk):
     return render(request, 'base/room.html', context)
 
 
+
+def userProfile(request, pk):
+    user = User.objects.get(id=pk)
+    rooms = user.room_set.all()
+    room_messages = user.message_set.all()
+    topics = Topic.objects.all()
+    context = {'user': user, 'rooms': rooms, 
+               'room_messages': room_messages, 'topics': topics}
+    return render(request, 'base/profile.html', context)
+
+
 @login_required(login_url='/login')
 def createRoom(request):
     form = RoomForm()
@@ -113,7 +124,9 @@ def createRoom(request):
     if request.method == 'POST':
         form = RoomForm(request.POST)
         if form.is_valid():
-            form.save()
+            room = form.save(commit=False)
+            room.host = request.user
+            room.save()
             return redirect('home')
 
     context = {'form': form}
